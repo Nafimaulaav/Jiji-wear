c1981b7c581d5ebb5b36952a536470333eeb823f
Your Name <nafymaula7@gmail.com>
2026-04-23 21:00:47 +0700

    MENGUBAH CONFIG MIDTRANS

--- config/midtrans.php
+++ config/midtrans.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'server_key' => env('SB-Mid-server-MSa6-swydoP1HRryWZFun0n6'),
-    'client_key' => env('SB-Mid-client-9sXo2n7u8l3m1h5'),
+    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY'),
     'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
     'is_sanitized' => env('MIDTRANS_IS_SANITIZED', true),
     'is_3ds' => env('MIDTRANS_IS_3DS', true),
